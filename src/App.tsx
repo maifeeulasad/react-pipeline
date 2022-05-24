f0441e4f4fd67d15757f7ac8f01a8fed96e0913d
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-05-24 09:26:13 +0600

    ui cleanun

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          React TS + Vite
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
