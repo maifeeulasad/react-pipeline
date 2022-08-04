8c44c63dfc58eb0d03decf5fbddb12163bc9eb30
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-08-04 19:37:47 +0600

    basic antd layout in every page

--- src/component/landing/Landing.tsx
+++ src/component/landing/Landing.tsx
@@ -1,8 +1,4 @@
 import React from 'react';
-import { PageHeader } from 'antd';
-import {
-  Link,
-} from 'react-router-dom';
 
 // import './Landing.css';
 
@@ -10,17 +6,7 @@ import logo from './landing.svg';
 
 const Landing = () => (
   <div>
-    <PageHeader
-      className="site-page-header"
-      title="Title"
-      subTitle="This is a subtitle"
-      extra={(
-        <>
-          <Link to="/page2">Page 2</Link>
-          <Link to="/page3">Page 3</Link>
-        </>
-)}
-    />
+
     <header>
       <img src={logo} className="animate-spin h-10" alt="logo" />
       <div className="text-red-600">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content:["./src/component/**/*.{ts,tsx}"],
+  content:["./src/component/**/*.{ts,tsx}", "./src/layout/*.{ts,tsx}"],
   theme: {
     extend: {},
   },
