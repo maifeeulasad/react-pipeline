473e40212febbad80fb5598f5d1072f18801b3ac
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-08-04 13:03:59 +0600

    pipeline//wip

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'antd/dist/antd.css';
-import { App } from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
