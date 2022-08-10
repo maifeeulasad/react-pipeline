4ebaee7ed279daa62c8fee8b61fea4c6d35db137
Maifee Ul Asad <maifeeulasad@gmail.com>
2022-08-10 13:35:36 +0600

    using dropdown menu as extra

--- src/layout/CustomLayout.tsx
+++ src/layout/CustomLayout.tsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import Layout from 'antd/lib/layout';
+import Menu from 'antd/lib/menu';
+import Dropdown from 'antd/lib/dropdown';
 import PageHeader from 'antd/lib/page-header';
 import { Link, useNavigate } from 'react-router-dom';
+import { DownOutlined } from '@ant-design/icons';
+
+import './CustomLayout.scss';
 
 const { Header, Content, Footer } = Layout;
+
+const CustomHeaderDropdownMenu = () => (
+  <Menu
+    items={[
+      {
+        key: '1',
+        label: (
+          <Link to="/page2">Page 2</Link>
+        ),
+      },
+      {
+        key: '2',
+        label: (
+          <Link to="/page3">Page 3</Link>
+        ),
+      },
+    ]}
+  />
+);
 
 const CustomHeader = () => {
   const navigate = useNavigate();
@@ -14,10 +38,9 @@ const CustomHeader = () => {
       subTitle="SPA using React + TS + Vite + Tailwind"
       onBack={() => navigate(-1)}
       extra={
-        <>
-          <Link to="/page2">Page 2</Link>
-          <Link to="/page3">Page 3</Link>
-        </>
+        <Dropdown overlay={CustomHeaderDropdownMenu}>
+          <DownOutlined />
+        </Dropdown>
   }
     />
   );
