006173a182f652ab899f954689060ac94aaedc96
Maifee Ul Asad <maifeeulasad@gmail.com>
2023-10-31 22:11:12 +0600

    [cypress]: added basic load test for our dev;

--- cypress/e2e/load.cy.ts
+++ cypress/e2e/load.cy.ts
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('loading webpage', () => {
+  it('load landing page', () => {
+    cy.visit('http://localhost:3000/react-pipeline')
   })
 })
