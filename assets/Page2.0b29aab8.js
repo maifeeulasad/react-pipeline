568ad50ca7dbf239f40b2a0a07bf2d30d3be88e5
maifeeulasad <maifeeulasad@users.noreply.github.com>
2023-10-31 16:01:01 +0000

    deploy: 16574617ad699dcad2e41e72893a46fcf3c3779c

--- assets/Page2.0b29aab8.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index.2308573e.js";const o=()=>a("div",{children:"Page 2"});export{o as default};
---
+++ assets/Page2.42aa4a45.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index.4f5dd9ee.js";const o=()=>a("div",{children:"Page 2"});export{o as default};
