c21cf84b48fae56ff8567e07c96049d1f63c9e0b
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-03-26 00:03:58 +0600

    [feature]: testing out new state management;

--- src/component/landing/Landing.tsx
+++ src/component/landing/Landing.tsx
@@ -3,6 +3,52 @@ import React from 'react';
 import styles from './Landing.module.scss';
 
 import logo from './landing.svg';
+import { usePersistentState } from '../../state/state';
+
+const ComponentIncrement = () => {
+  const [count, setCount] = usePersistentState('app/counter', 0);
+  return (
+    <div style={{ marginBottom: '1rem' , border: '1px solid black', padding: '1rem' }}>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+      <div>Count: {count}</div>
+    </div>
+  );
+}
+
+const ComponentDecrement = () => {
+  const [count, setCount] = usePersistentState('app/counter', 0);
+  return (
+    <div style={{ marginBottom: '1rem' , border: '1px solid black', padding: '1rem' }}>
+      <button onClick={() => setCount(count - 1)}>
+        Decrement
+      </button>
+      <div>Count: {count}</div>
+    </div>
+  );
+}
+
+const ComponentRandom = () => {
+  const [random, setRandom] = usePersistentState('app/counter', 0);
+  return (
+    <div style={{ marginBottom: '1rem' , border: '1px solid black', padding: '1rem' }}>
+      <button onClick={() => setRandom(Math.random())}>
+        Set Random
+      </button>
+      <div>Count: {random}</div>
+    </div>
+  );
+}
+
+const ComponentOnlyRead = () => {
+  const [count] = usePersistentState('app/counter', 0);
+  return (
+    <div style={{ marginBottom: '1rem' , border: '1px solid black', padding: '1rem' }}>
+      <div>Count: {count}</div>
+    </div>
+  );
+}
 
 const Landing = () => (
   <div>
@@ -14,6 +60,16 @@ const Landing = () => (
       </div>
       <div className={styles.sassExample}>
         sass is here for styling
+      </div>
+      <div>
+        <div style={{display:'flex'}}>
+          <ComponentIncrement />
+          {/* <ComponentDecrement /> */}
+        </div>
+        <div style={{display:'flex'}}>
+          {/* <ComponentRandom /> */}
+          {/* <ComponentOnlyRead /> */}
+        </div>
       </div>
     </header>
   </div>
