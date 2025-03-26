739af6d4c791470761763b58cd03a1ba095ac9a7
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-03-27 04:46:48 +0600

    [puppeteer]: update w no sandbox for puppeteer;

--- puppeteer/snap.js
+++ puppeteer/snap.js
@@ -7,7 +7,13 @@ function delay(time) {
  }
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    slowMo: 50
+    });
+  
   const page = await browser.newPage();
   await page.goto('http://localhost:5173/react-pipeline/');
   await delay(2000);
