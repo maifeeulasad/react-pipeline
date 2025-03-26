04aa2fd87d4bce18ce03a0b3339d243b84a13526
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-03-26 23:29:58 +0600

    [automation,e2e,script]: to generate snap of homepage;

---
+++ puppeteer/snap.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer');
+
+function delay(time) {
+    return new Promise(function(resolve) { 
+        setTimeout(resolve, time)
+    });
+ }
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:5173/react-pipeline/');
+  await delay(2000);
+  await page.screenshot({ path: 'homepage.png' });
+
+  await browser.close();
+})();
