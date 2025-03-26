5287e4f676d54bfd49e45cbbf9273fa7ca39de1c
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-03-27 04:47:17 +0600

    [chores,lint]: linting;

--- puppeteer/snap.js
+++ puppeteer/snap.js
@@ -1,23 +1,23 @@
 const puppeteer = require('puppeteer');
 
 function delay(time) {
-    return new Promise(function(resolve) { 
+    return new Promise(function (resolve) {
         setTimeout(resolve, time)
     });
- }
+}
 
 (async () => {
-  const browser = await puppeteer.launch({
-    executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    slowMo: 50
+    const browser = await puppeteer.launch({
+        executablePath: process.env.PUPPETEER_EXEC_PATH, // set by docker container
+        headless: true,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        slowMo: 50
     });
-  
-  const page = await browser.newPage();
-  await page.goto('http://localhost:5173/react-pipeline/');
-  await delay(2000);
-  await page.screenshot({ path: 'homepage.png' });
 
-  await browser.close();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:5173/react-pipeline/');
+    await delay(2000);
+    await page.screenshot({ path: 'homepage.png' });
+
+    await browser.close();
 })();
