13c8667e4c68457e6beb24c5116c343ab4e4dc0f
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-03-26 22:15:41 +0600

    [ux.ui]: centered content in landing page;

--- src/component/landing/Landing.tsx
+++ src/component/landing/Landing.tsx
@@ -5,10 +5,9 @@ import styles from './Landing.module.scss';
 import logo from './landing.svg';
 
 const Landing = () => (
-  <div>
-
+  <div style={{ textAlign: 'center' }}>
     <header>
-      <img src={logo} className="animate-spin h-10" alt="logo" />
+      <img src={logo} className="animate-spin h-10 mx-auto" alt="logo" />
       <div className="text-red-600">
         React + TS + Vite + Tailwind
       </div>
