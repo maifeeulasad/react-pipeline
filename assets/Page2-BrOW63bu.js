a5ffb4535e7cc645691062fd71b02248160feceb
maifeeulasad <maifeeulasad@users.noreply.github.com>
2025-03-26 05:30:45 +0000

    deploy: a3e3d9941b50a03f09f71c0ce87696f9780cf942

---
+++ assets/Page2-BrOW63bu.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-BqM5vfRb.js";const o=()=>a("div",{children:"Page 2"});export{o as default};
--- assets/Page2.6acc9ed6.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index.0d8f4e7e.js";const o=()=>a("div",{children:"Page 2"});export{o as default};
