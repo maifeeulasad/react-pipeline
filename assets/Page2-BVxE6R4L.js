8236eaba1514a3001bc5d019beb7f4cb14861567
maifeeulasad <maifeeulasad@users.noreply.github.com>
2025-04-01 07:37:07 +0000

    deploy: a16ad61b97c0763327b968e17f60cbf413c90392

--- assets/Page2-BVxE6R4L.js
+++
@@ -1,1 +0,0 @@
-import{u as r}from"./state-ClsMfsNq.js";import{a as e}from"./index-BlTU1Kx8.js";const s=()=>{const[t]=r("global/counter",0);return e("div",{children:["Page 2",e("div",{style:{border:"1px solid red",padding:"10px"},children:["global/counter state: ",t]})]})};export{s as default};
