8539f700c484568a864b8ca4b1a9a1e73f151fd8
maifeeulasad <maifeeulasad@users.noreply.github.com>
2025-04-01 17:21:04 +0000

    deploy: 43b1481a24296a265b8319feb531bfe7f7a412d7

---
+++ assets/Page2-BmloQHMQ.js
@@ -0,0 +1,1 @@
+import{e as r}from"./index-Das6nGwR.js";import{a as e}from"./index-DBIqplOB.js";const a=()=>{const[t]=r.usePersistentState("global/counter",0);return e("div",{children:["Page 2",e("div",{style:{border:"1px solid red",padding:"10px"},children:["global/counter state: ",t]})]})};export{a as default};
--- assets/Page2-v8VVctGW.js
+++
@@ -1,1 +0,0 @@
-import{u as r}from"./state-D29PNkXD.js";import{a as e}from"./index-ZI7Bts3D.js";const s=()=>{const[t]=r("global/counter",0);return e("div",{children:["Page 2",e("div",{style:{border:"1px solid red",padding:"10px"},children:["global/counter state: ",t]})]})};export{s as default};
