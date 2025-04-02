d3b2116706c89e7ce83b98521e2178e30cc4288b
maifeeulasad <maifeeulasad@users.noreply.github.com>
2025-04-02 14:28:29 +0000

    deploy: 2675b67dbbe2f82d7a599ff4a513b5b3cd402efd

--- assets/Page2-BKKPQr_x.js
+++
@@ -1,1 +0,0 @@
-import{e as r}from"./index-Brf08Qz2.js";import{a as e}from"./index-Dx35Bazg.js";const a=()=>{const[t]=r.usePersistentState("global/counter",0);return e("div",{children:["Page 2",e("div",{style:{border:"1px solid red",padding:"10px"},children:["global/counter state: ",t]})]})};export{a as default};
