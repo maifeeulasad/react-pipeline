d8389af0cb493ca5e1d995a198fa2d43569ad542
maifeeulasad <maifeeulasad@users.noreply.github.com>
2025-04-29 06:49:51 +0000

    deploy: dd60f995905b30b033b62a8fac5a3780a9d8bd24

--- assets/Page2-CuJUDnf6.js
+++
@@ -1,1 +0,0 @@
-import{e as r}from"./index-CfwJSooJ.js";import{a as e}from"./index-CxLj7vZj.js";const a=()=>{const[t]=r.usePersistentState("global/counter",0);return e("div",{children:["Page 2",e("div",{style:{border:"1px solid red",padding:"10px"},children:["global/counter state: ",t]})]})};export{a as default};
