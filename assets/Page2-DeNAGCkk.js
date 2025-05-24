2b59034b34a58d5319ce120c28a97b3742d983a6
maifeeulasad <maifeeulasad@users.noreply.github.com>
2025-05-24 04:52:32 +0000

    deploy: cd6ac40b76c1471f24a80776912642df0c939c44

--- assets/Page2-DeNAGCkk.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-ChH2VFkt.js";import{e as s}from"./index-DQ06aFxF.js";const n=()=>{const[t]=s.usePersistentState("global/counter",0);return e.jsxs("div",{children:["Page 2",e.jsxs("div",{style:{border:"1px solid red",padding:"10px"},children:["global/counter state: ",t]})]})};export{n as default};
